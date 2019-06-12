cebdf08a1bac082edea43b8eab8e1e90e5b4c652
kadoshita <sublimer.me@gmail.com>
2019-06-12 13:19:12 +0000

    全文検索関連のカスタマイズ
    # Conflicts:
    #	app/javascript/mastodon/actions/search.js

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -41,6 +41,8 @@ class SearchService < BaseService
       definition = definition.filter(term: { account_id: @options[:account_id] })
     end
 
+    definition = definition.order(id:{order:'desc'})
+
     if @options[:min_id].present? || @options[:max_id].present?
       range      = {}
       range[:gt] = @options[:min_id].to_i if @options[:min_id].present?
