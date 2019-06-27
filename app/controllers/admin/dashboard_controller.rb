33b673ba333a820f84ed0856a375b1d17dbcf2f1
kadoshita <sublimer.me@gmail.com>
2019-06-27 07:39:23 +0000

    管理者ダッシュボードにMinioのディスク使用量を表示

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 require 'sidekiq/api'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'date'
 
 module Admin
   class DashboardController < BaseController
@@ -22,6 +26,7 @@ module Admin
       @recent_users          = User.confirmed.recent.includes(:account).limit(4)
       @database_size         = ActiveRecord::Base.connection.execute('SELECT pg_database_size(current_database())').first['pg_database_size']
       @redis_size            = redis_info['used_memory']
+      @minio_size            = get_minio_storage_used()
       @ldap_enabled          = ENV['LDAP_ENABLED'] == 'true'
       @cas_enabled           = ENV['CAS_ENABLED'] == 'true'
       @saml_enabled          = ENV['SAML_ENABLED'] == 'true'
@@ -40,6 +45,73 @@ module Admin
 
     def redis_info
       @redis_info ||= Redis.current.info
+    end
+
+    def get_minio_storage_used
+      token = auth_req()
+      return get_storage_info(token)
+    end
+
+    def get_x_amz_date
+      return Time.now.strftime('%Y%m%dT%H%M%SZ')
+    end
+
+    def auth_req
+      minio_url = ENV['S3_ENDPOINT'] + '/minio/webrpc'
+      access_id = ENV['AWS_ACCESS_KEY_ID']
+      access_key = ENV['AWS_SECRET_ACCESS_KEY']
+      uri = URI.parse(minio_url)
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = 'application/json'
+      request['X-Amz-Date'] = get_x_amz_date()
+      request.body = JSON.dump({
+        'id' => 1,
+        'jsonrpc' => '2.0',
+        'params' => {
+          'username' => access_id,
+          'password' => access_key
+        },
+        'method' => 'Web.Login'
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == 'https',
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      result=JSON.parse(response.body);
+
+      return result['result']['token']
+    end
+
+    def get_storage_info(token)
+      minio_url = ENV['S3_ENDPOINT'] + '/minio/webrpc'
+      uri = URI.parse(minio_url)
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = 'application/json'
+      request['Authorization'] = 'Bearer '+token
+      request['X-Amz-Date'] = get_x_amz_date()
+      request.body = JSON.dump({
+        'id' => 1,
+        'jsonrpc' => '2.0',
+        'params' => {},
+        'method' => 'Web.StorageInfo'
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == 'https',
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      result=JSON.parse(response.body);
+
+      return result['result']['storageInfo']['Used']
     end
   end
 end
